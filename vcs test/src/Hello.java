40d917b519ef649953b6610ab29317c5e715b308
GGG <GGG@DESKTOP-8N3GG4O>
2023-05-25 10:16:55 +0900

    aaa

--- vcs test/src/Hello.java
+++ vcs test/src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
 	public static void main(String[] args) {
+		// 로컬에서 작업했습니다.
 		System.out.println("Hello world");
 		
 		System.out.println("변경");
@@ -7,7 +8,5 @@ public class Hello {
 		System.out.println("세번째 커밋");
 		
 		// 주석
-		
-		// 로컬에서 작업했습니다.
 	}
 }
